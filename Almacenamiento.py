29d7e632f14445239399d2886586792635811edf
Davidgp04 <104110970+Davidgp04@users.noreply.github.com>
2022-08-24 07:13:47 -0500

    Update Almacenamiento.py

--- Almacenamiento.py
+++ Almacenamiento.py
@@ -1,12 +1,2 @@
-try:
-    calles=[]
-    archivo=open('calles_de_medellin_con_acoso.csv','r',encoding='utf8')
-    for line in archivo:
-        line=line.replace('\n','')
-        line=line.split(';')
-        calles.append(line)
-    print(calles[1])
-except Exception as e:
-    print(f'ocurrió un error {e}')
-finally:
-    archivo.close()
+import pandas as pd
+df=pd.read_csv('calles_de_medellin_con_acoso.csv', sep=';')
